a88407dde9e57e48d9b6b81256f0c3cb71f60197
Muhammed Aflah <amaflu4424@gmail.com>
2026-02-24 23:54:30 +0530

    .

--- src/components/RoomModal.tsx
+++ src/components/RoomModal.tsx
@@ -8,8 +8,8 @@ type RoomModalProps = {
   
   const RoomModal = (props : RoomModalProps) => {
     return (
-      <div className='absolute inset-0 bg-black/70 z-99 flex justify-center items-center'> 
-        <div className='relative bg-black border-white border w-1/3 h-1/3 flex  flex-col items-center justify-center rounded-lg'>
+      <div className='absolute inset-0  z-99 flex justify-center items-center'> 
+        <div className='relative bg-black border-white border w-1/3 h-1/3 flex  flex-col items-center rounded-2xl justify-center '>
         <X className=' text-white absolute top-5 right-5 cursor-pointer hover:text-gray-500' onClick={props.closeInput}/>
             <input type="number" placeholder='Enter Room Id ...' />
             <button className='reset-btn'>Submit Id</button>
