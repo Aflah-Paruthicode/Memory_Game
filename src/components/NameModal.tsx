70a780b349a6755430b4b6084c7f3627dd6d791c
Muhammed Aflah <amaflu4424@gmail.com>
2026-02-25 21:24:45 +0530

    name modal and room id modal styled

--- src/components/NameModal.tsx
+++ src/components/NameModal.tsx
@@ -2,42 +2,28 @@ import { useState } from "react";
 import { useAppContext } from "../context/useAppContext";
 
 const NameModal = () => {
-  const { setName } = useAppContext(); 
+  const { setName } = useAppContext();
   const [localName, setLocalName] = useState("");
   const [err, setErr] = useState("");
 
   const handleAddName = () => {
-    if (localName.trim() === "") {
-      setErr("Name can't be null");
-      return;
-    }
-
-    if (localName.length > 7) {
-      setErr("Name can't be long");
-      return;
-    }
-
-    if (localName.length < 4) {
-      setErr("At least 4 characters required");
-      return;
-    }
-
+    if (localName.trim() === "") return setErr("Name can't be null");
+    if (localName.length > 7) return setErr("Name can't be long");
+    if (localName.length < 4) return setErr("At least 4 characters required");
     setName(localName);
   };
 
   return (
-    <div className="lobby-container absolute">
-      <h1>Enter your name!</h1>
+    <div className="lobby-container absolute inset-0  z-99 flex justify-center items-center">
+      <div className="relative bg-[#0f172ae6] border-[#ffffff1a] border min-w-xl h-56 flex  flex-col items-center gap-3 rounded-2xl justify-center ">
+        <h1 className="text-2xl font-medium">Enter your name!</h1>
 
-      <input
-        placeholder="Enter Name..."   
-        value={localName}
-        onChange={(e) => setLocalName(e.target.value)}
-      />
+        <input className="bg-gray-800 h-10 w-1/2 rounded-full" placeholder="Enter Name..." value={localName} onChange={(e) => setLocalName(e.target.value)} />
 
-      <button onClick={handleAddName}>Join Game</button>
+        {err && <p className="text-red-700">{err}</p>}
+        <button className="reset-btn" onClick={handleAddName}>Join Game</button>
 
-      {err && <p>{err}</p>}
+      </div>
     </div>
   );
 };
--- src/components/RoomModal.tsx
+++ src/components/RoomModal.tsx
@@ -9,8 +9,8 @@ type RoomModalProps = {
   const RoomModal = (props : RoomModalProps) => {
     return (
       <div className='absolute inset-0  z-99 flex justify-center items-center'> 
-        <div className='relative bg-black border-white border w-1/3 h-1/3 flex  flex-col items-center rounded-2xl justify-center '>
-        <X className=' text-white absolute top-5 right-5 cursor-pointer hover:text-gray-500' onClick={props.closeInput}/>
+        <div className='relative bg-[#0f172ae6] border-[#ffffff1a] backdrop-blur-sm border w-1/3 h-1/3 flex  flex-col items-center rounded-2xl justify-center '>
+        <X className=' text-[#ffffff1a] absolute top-5 right-5 cursor-pointer hover:text-gray-500' onClick={props.closeInput}/>
             <input type="number" placeholder='Enter Room Id ...' />
             <button className='reset-btn'>Submit Id</button>
         </div>
