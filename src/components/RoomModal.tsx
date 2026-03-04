6e0eebd3e1f15a77e15ff0c8a6f6d6e21a9b66b3
Muhammed Aflah <amaflu4424@gmail.com>
2026-03-04 23:21:41 +0530

    ,

--- src/components/RoomModal.tsx
+++ src/components/RoomModal.tsx
@@ -9,11 +9,11 @@ const RoomModal = (props: RoomModalProps) => {
   const [roomId, setRoomId] = useState("");
   const [err, setErr] = useState("");
 
-  const handleCreateRoom = () => {
+  const handleCreateRoom = () => { 
     if (roomId.length !== 6) return setErr("Enter 6 digits to continue!"); 
     setRoomId(roomId); 
   }; 
-
+ 
   return (
     <div className="absolute inset-0  z-99 flex justify-center items-center "> 
       <div className="relative bg-[#0f172ae6] border-[#ffffff1a] backdrop-blur-sm border w-1/3 h-1/3 flex gap-3  flex-col items-center rounded-2xl justify-center ">
